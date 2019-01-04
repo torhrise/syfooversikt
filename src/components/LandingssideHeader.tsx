4a1d44127b667a4bb4e03b580dd5a1d28469dd8c
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-01-04 15:15:21 +0100

    Test klikk-effekt, og decorator
    SYFOUTV-2505

--- src/components/LandingssideHeader.tsx
+++ src/components/LandingssideHeader.tsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { Landingsside } from '../sider/Landingsside';
+
+function hei() {
+    setTimeout(() => {
+        render(<Landingsside />, document.getElementById('maincontent') as HTMLElement);
+    }, 500);
+    render(<LandingssideHeader bilde={'/syfooversikt/src/img/veileder.svg'} />, document.getElementById('maincontent') as HTMLElement);
+}
 
 interface LandingssideHeaderProps {
     bilde: string;
@@ -8,7 +17,7 @@ const LandingssideHeader = (props: LandingssideHeaderProps) => {
     const { bilde } = props;
     return (<div className="landingsside__header">
         <h1>Syfooversikt</h1>
-        <img className="header__bilde" src={bilde} alt="veileder" />
+        <img className="header__bilde" src={bilde} alt="veileder" onClick={hei}/>
     </div>);
 };
 
