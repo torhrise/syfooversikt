24081804ed6fe90dc1f1bb0d5ccdb46649db080c
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-01-07 16:07:54 +0100

    Fiks test/test-rammeverk
    SYFOUTV-2505

--- src/test/Landingsside.test.tsx
+++ src/test/Landingsside.test.tsx
@@ -1,5 +1,20 @@
+import chaiEnzyme from 'chai-enzyme';
 import React from 'react';
+import chai from 'chai';
+import { shallow } from 'enzyme';
+import LandingssideHeader from '../components/LandingssideHeader';
+import { Landingsside } from '../sider/Landingsside';
 
-it('skal sette x = 1', () => {
-  const x = 1;
+chai.use(chaiEnzyme());
+const expect = chai.expect;
+
+describe('Landingsside', () => {
+  const component = shallow(<Landingsside />);
+  it('Skal rendre LandingssideHeader', () => {
+    expect(
+      component.contains(
+        <LandingssideHeader bilde={'/syfooversikt/src/img/veileder.svg'} />
+      )
+    ).to.equal(true);
+  });
 });
