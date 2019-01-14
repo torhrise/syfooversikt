52213087861e3a6d11bb751a37a2a0f7ab819839
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-01-14 15:46:46 +0100

    Test decorator
    SYFOUTV-2522

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,18 @@ import React from 'react';
 import { Landingsside } from './sider/Landingsside';
 import './styles/styles.css';
 
+const config = {
+  config: {
+    toggles: {
+      visEnhet: true,
+      visSokefelt: true,
+      visVeileder: true,
+    },
+    fnr: '17086800071',
+    applicationName: 'Syfooversikt',
+  },
+};
+
+(window as any).renderDecoratorHead(config);
+
 render(<Landingsside />, document.getElementById('maincontent') as HTMLElement);
