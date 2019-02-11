60f8eefe9def15b4588e308f19f6b9a7d9f29612
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2019-02-11 13:12:15 +0100

    F

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,9 +1,3 @@
-const getCookie = (name: string) => {
-  const re = new RegExp(`${name}=([^;]+)`);
-  const match = re.exec(document.cookie);
-  return match !== null ? match[1] : '';
-};
-
 const createLogger = () => {
   if (window.location.search.indexOf('log=true') > -1 || (process.env.NODE_ENV === 'development')) {
     // tslint:disable-next-line
@@ -15,49 +9,89 @@ const createLogger = () => {
 
 const log = createLogger();
 
+export const hentLoginUrl = () => {
+    if (window.location.href.indexOf('app.adeo.no') > -1) {
+        // Prod
+        return 'https://loginservice.nais.adeo.no/login';
+    }
+    // Preprod
+    return 'https://loginservice.nais.preprod.local/login';
+};
+
+export const hentRedirectBaseUrl = (windowLocationHref: string) => {
+    if (window.location.href.indexOf('app.adeo.no') > -1) {
+        // Prod
+        if (windowLocationHref.indexOf('sykefravaer') > -1) {
+            return 'https://modiasyfofront.nais.adeo.no';
+        } else if (windowLocationHref.indexOf('sykefravaersoppfoelging') > -1) {
+            return 'https://sykefravaersoppfoelgingfront.nais.adeo.no';
+        }
+    }
+    if (windowLocationHref.indexOf('sykefravaer') > -1) {
+        return 'https://modiasyfofront-q1.nais.preprod.local';
+    } else if (windowLocationHref.indexOf('sykefravaersoppfoelging') > -1) {
+        return 'https://sykefravaersoppfoelgingfront-q1.nais.preprod.local';
+    }
+    return windowLocationHref;
+};
+
+export const lagreRedirectUrlILocalStorage = (href: string) => {
+    localStorage.setItem('redirecturl', href);
+};
+
 export function get(url: string) {
-  return fetch(url, {
-    credentials: 'include',
-  })
-    .then((res) => {
-      if (res.status === 404) {
-        throw new Error('404');
-      } else if (res.status === 403) {
-        return res.json();
-      }
-      if (res.status > 400) {
-        throw new Error('Det oppstod en feil');
-      }
-      return res.json();
+    const fetchX = window.fetch;
+    return fetchX(url, {
+        credentials: 'include',
     })
-    .catch((err) => {
-      log(err);
-      throw err;
-    });
+        .then((res) => {
+            if (res.status === 401) {
+                log(res, 'Redirect til login');
+                lagreRedirectUrlILocalStorage(window.location.href);
+                window.location.href = `${hentLoginUrl()}?redirect=${hentRedirectBaseUrl(window.location.href)}`;
+            } else if (res.status > 400) {
+                log(res);
+                throw new Error('Forespørsel feilet');
+            }
+            return res.json();
+        })
+        .catch((err) => {
+            log(err);
+            throw err;
+        });
 }
 
 export function post(url: string, body: object) {
-  return fetch(url, {
-    credentials: 'include',
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-      'NAV_CSRF_PROTECTION': getCookie('NAV_CSRF_PROTECTION'),
-    },
-  })
-    .then((res) => {
-      if (res.status > 400) {
-        log(res);
-        throw new Error('Forespørsel feilet');
-      } else {
-        return res;
-      }
+    const fetchX = window.fetch;
+    return fetchX(url, {
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+        }),
     })
-    .catch((err) => {
-      log(err);
-      throw err;
-    });
+        .then((res) => {
+            if (res.status === 401) {
+                log(res, 'Redirect til login');
+                lagreRedirectUrlILocalStorage(window.location.href);
+                window.location.href = `${hentLoginUrl()}?redirect=${hentRedirectBaseUrl(window.location.href)}`;
+                return null;
+            } else if (res.status > 400) {
+                log(res);
+                throw new Error('Forespørsel feilet');
+            } else {
+                const contentType = res.headers.get('Content-Type') || '';
+                if (contentType.includes('json')) {
+                    return res.json();
+                }
+                return res;
+            }
+        })
+        .catch((err) => {
+            log(err);
+            throw err;
+        });
 }
 
 export function getWithoutThrows(url: string) {
