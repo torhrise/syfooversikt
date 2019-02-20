106ea4a9051813c5d844b60aa007a883d5799257
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2019-02-20 09:02:39 +0100

    Redirect til loginservice hvis man ikke er logget inn fra for

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -15,60 +15,92 @@ const createLogger = () => {
 
 const log = createLogger();
 
+export const hentLoginUrl = () => {
+    if (window.location.href.indexOf('app.adeo.no') > -1) {
+        // Prod
+        return 'https://loginservice.nais.adeo.no/login';
+    }
+    // Preprod
+    return 'https://loginservice.nais.preprod.local/login';
+};
+
+export const hentRedirectBaseUrl = (windowLocationHref: string) => {
+    if (window.location.href.indexOf('app.adeo.no') > -1) {
+        return 'https://syfooversikt.nais.adeo.no';
+    }
+    return 'https://syfooversikt-q1.nais.preprod.local';
+
+};
+
+export const lagreRedirectUrlILocalStorage = (href: string) => {
+    localStorage.setItem('redirecturl', href);
+};
+
 export function get(url: string) {
-  return fetch(url, {
-    credentials: 'include',
-  })
-    .then((res) => {
-      if (res.status === 404) {
-        throw new Error('404');
-      } else if (res.status === 403) {
-        return res.json();
-      }
-      if (res.status > 400) {
-        throw new Error('Det oppstod en feil');
-      }
-      return res.json();
+    const fetchX = window.fetch;
+    return fetchX(url, {
+        credentials: 'include',
     })
-    .catch((err) => {
-      log(err);
-      throw err;
-    });
+        .then((res) => {
+            if (res.status === 401) {
+                log(res, 'Redirect til login');
+                lagreRedirectUrlILocalStorage(window.location.href);
+                window.location.href = `${hentLoginUrl()}?redirect=${hentRedirectBaseUrl(window.location.href)}`;
+            } else if (res.status > 400) {
+                log(res);
+                throw new Error('Forespørsel feilet');
+            }
+            return res.json();
+        })
+        .catch((err) => {
+            log(err);
+            throw err;
+        });
 }
 
 export function post(url: string, body: object) {
-  return fetch(url, {
-    credentials: 'include',
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-      'NAV_CSRF_PROTECTION': getCookie('NAV_CSRF_PROTECTION'),
-    },
-  })
-    .then((res) => {
-      if (res.status > 400) {
-        log(res);
-        throw new Error('Forespørsel feilet');
-      } else {
-        return res;
-      }
+    const fetchX = window.fetch;
+    return fetchX(url, {
+        credentials: 'include',
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'NAV_CSRF_PROTECTION': getCookie('NAV_CSRF_PROTECTION'),
+        }),
     })
-    .catch((err) => {
-      log(err);
-      throw err;
-    });
+        .then((res) => {
+            if (res.status === 401) {
+                log(res, 'Redirect til login');
+                lagreRedirectUrlILocalStorage(window.location.href);
+                window.location.href = `${hentLoginUrl()}?redirect=${hentRedirectBaseUrl(window.location.href)}`;
+                return null;
+            } else if (res.status > 400) {
+                log(res);
+                throw new Error('Forespørsel feilet');
+            } else {
+                const contentType = res.headers.get('Content-Type') || '';
+                if (contentType.includes('json')) {
+                    return res.json();
+                }
+                return res;
+            }
+        })
+        .catch((err) => {
+            log(err);
+            throw err;
+        });
 }
 
 export function getWithoutThrows(url: string) {
-  return fetch(url, {
-    credentials: 'include',
-  })
-    .then((res) => {
-      return res.json();
+    return fetch(url, {
+        credentials: 'include',
     })
-    .catch((err) => {
-      log(err);
-      throw err;
-    });
+        .then((res) => {
+            return res.json();
+        })
+        .catch((err) => {
+            log(err);
+            throw err;
+        });
 }
