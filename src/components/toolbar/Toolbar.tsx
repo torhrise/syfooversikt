d03ab89f5b0831133d8762712268549a6fdf7c8d
JMLindseth-nav <40055758+JMLindseth-nav@users.noreply.github.com>
2019-03-28 15:31:08 +0100

    Oppgrader patch automatisk for react og prop-types
    Legg på syntakssukker, bruk em i stedet for rem i less

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import TildelVeileder from './TildelVeileder';
 
-const Toolbar = () => (
-  <section className="toolbar blokk-xs">
-    <div className="toolbar__element toolbar__venstre toolbar--skille-mellom-elementer">
-      <TildelVeileder />
-    </div>
-  </section>
-);
+const Toolbar = () => (<section className="toolbar blokk-xs">
+  <div className="toolbar__element toolbar--skille-mellom-elementer">
+    <TildelVeileder />
+  </div>
+</section>);
 
 export default Toolbar;
