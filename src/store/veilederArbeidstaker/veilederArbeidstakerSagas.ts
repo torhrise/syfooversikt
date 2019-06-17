5bd0c80873613d234a3760bf8ab04c09dd66c1ac
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-17 09:16:54 +0200

    TC

--- src/store/veilederArbeidstaker/veilederArbeidstakerSagas.ts
+++ src/store/veilederArbeidstaker/veilederArbeidstakerSagas.ts
@@ -5,7 +5,10 @@ import {
   put,
   takeEvery,
 } from 'redux-saga/effects';
-import { post } from '../../api/index';
+import {
+    get,
+    post,
+} from '../../api/index';
 import * as actions from './veilederArbeidstaker_actions';
 import { fullNaisUrlDefault } from '../../utils/miljoUtil';
 import { HOST_NAMES } from '../../konstanter';
@@ -26,6 +29,23 @@ export function* pushBrukerArbeidstakerSaga(
   }
 }
 
+export function* hentBrukerArbeidstakerSaga() {
+    try {
+        const host = HOST_NAMES.SYFOOVERSIKTSRV;
+        const path = `${process.env.REACT_APP_SYFOOVERSIKTSRVREST_ROOT}/persontildeling/veileder/Z990623`;
+        yield call(get, fullNaisUrlDefault(host, path));
+    } catch (e) {
+        yield put(actions.pushVeilederArbeidstakerFeilet());
+    }
+}
+
+function* watchPushVeilederArbeidstakerTest() {
+    yield takeEvery(
+        actions.veilederArbeidstakerActionTypes.PUSH_VEILEDERARBEIDSTAKER_FORESPURT,
+        hentBrukerArbeidstakerSaga
+    );
+}
+
 function* watchPushVeilederArbeidstaker() {
   yield takeEvery(
       actions.veilederArbeidstakerActionTypes.PUSH_VEILEDERARBEIDSTAKER_FORESPURT,
@@ -35,6 +55,7 @@ function* watchPushVeilederArbeidstaker() {
 
 export default function* veilederArbeidstakerSagas() {
   yield all([
-    fork(watchPushVeilederArbeidstaker)
+    fork(watchPushVeilederArbeidstaker),
+    fork(watchPushVeilederArbeidstakerTest)
   ]);
 }
