a12bbbf7bec2dfb6c0c152723b003c3ed5b56547
Kristian Storvoll <kristian.storvoll@nav.no>
2019-07-24 16:23:15 +0200

    Formater brukerens navn med format: Etternavn, Fornavn

--- src/utils/lenkeUtil.tsx
+++ src/utils/lenkeUtil.tsx
@@ -7,8 +7,23 @@ const lenkeTilModiaBasertPaaFnr = (fnr: string) => {
   return fullAppAdeoUrl(path);
 };
 
+const formaterNavn = (navn?: string) => {
+  if (!navn) return '';
+  // tslint:disable-next-line: no-console
+  console.log(navn);
+  const arr = navn.split(' ');
+  const etternavn = arr.pop();
+  if (arr.length > 0) {
+    const fornavn = arr.join(' ');
+    return `${fornavn}, ${etternavn}`;
+  }
+  // tslint:disable-next-line: no-console
+  console.log(arr);
+  return etternavn;
+};
+
 export const lenkeTilModiaEnkeltperson = (navn: string, fnr: string) => {
   return (<Lenke href={lenkeTilModiaBasertPaaFnr(fnr)}>
-      {navn}
+      {formaterNavn(navn)}
   </Lenke>);
 };
