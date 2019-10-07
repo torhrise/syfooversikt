f920bd9b2085db90f701a9ace9e548b2419e94fe
Kristian Storvoll <kristian.storvoll@nav.no>
2019-10-07 16:33:47 +0200

    State check for loaded data before rendering veileder name (#73)
    State check for loaded data before rendering veileder name

--- src/components/Personrad.tsx
+++ src/components/Personrad.tsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import {
-  Column,
-  Row,
-  RowProps,
-} from 'nav-frontend-grid';
+import { Column } from 'nav-frontend-grid';
 import { Checkbox } from 'nav-frontend-skjema';
 import themes from '../styles/themes';
 import { lenkeTilModiaEnkeltperson } from '../utils/lenkeUtil';
 import { PersonData } from '../store/personregister/personregisterTypes';
 import {
   skjermingskode,
-  veilederEllerUfordelt,
 } from '../utils/personDataUtil';
-import { Veileder } from '../store/veiledere/veiledereTypes';
 
 interface PersonradProps {
   fnr: string;
   personData: PersonData;
   checkboxHandler: (fnr: string) => void;
   kryssAv: boolean;
-  veileder?: Veileder;
+  veilederName: string;
   index: number;
 }
 
@@ -44,38 +38,32 @@ const VelgBoks = styled(Checkbox)`
   padding-bottom: 1em;
 `;
 
-class Personrad extends Component<PersonradProps> {
-  constructor(props: PersonradProps) {
-    super(props);
-  }
+export default (props: PersonradProps) => {
+  const {
+    fnr,
+    checkboxHandler,
+    personData,
+    kryssAv,
+    veilederName,
+    index,
+  } = props;
 
-  render() {
-    const {
-      fnr,
-      checkboxHandler,
-      personData,
-      kryssAv,
-      veileder,
-      index,
-    } = this.props;
-    return (
-      <PersonRad index={index} selected={kryssAv}>
-        <Column xs={'1'}>
-          <VelgBoks
-            label={''}
-            checked={!!kryssAv}
-            onChange={(event) => {
-              checkboxHandler(fnr);
-            }}
-          />
-        </Column>
-        <Column className="personrad__navn" xs={'3'}>{lenkeTilModiaEnkeltperson(personData.navn, fnr)}</Column>
-        <Column className="personrad__fnr" xs={'2'}>{fnr}</Column>
-        <Column className="personrad__veileder" xs={'2'}>{personData.tildeltVeilederIdent}</Column>
-        <Column className="personrad__veiledernavn" xs={'2'}>{veilederEllerUfordelt(veileder)}</Column>
-        <Column className="personrad__skjermet" xs={'2'}>{skjermingskode(personData)}</Column>
-      </PersonRad>);
-  }
-}
-
-export default Personrad;
+  return (
+    <PersonRad index={index} selected={kryssAv}>
+      <Column xs={'1'}>
+        <VelgBoks
+          label={''}
+          checked={!!kryssAv}
+          onChange={(event) => {
+            checkboxHandler(fnr);
+          }}
+        />
+      </Column>
+      <Column className="personrad__navn" xs={'3'}>{lenkeTilModiaEnkeltperson(personData.navn, fnr)}</Column>
+      <Column className="personrad__fnr" xs={'2'}>{fnr}</Column>
+      <Column className="personrad__veileder" xs={'2'}>{personData.tildeltVeilederIdent}</Column>
+      <Column className="personrad__veiledernavn" xs={'2'}>{veilederName}</Column>
+      <Column className="personrad__skjermet" xs={'2'}>{skjermingskode(personData)}</Column>
+    </PersonRad>
+  );
+};
