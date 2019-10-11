8bb302c8072f3262cd77534bf76660dc4e2814cb
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-10-11 12:39:22 +0200

    Add missing semicolon

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const setupMetrics = () => {
     register.registerMetric(counters.userFilterMoteplanleggerCounter);
     register.registerMetric(counters.userFilterUfordelteCounter);
     register.registerMetric(counters.userFilterIkkeAktivitetCounter);
-    register.registerMetric(counters.userFilterVeilederSearchCounter)
+    register.registerMetric(counters.userFilterVeilederSearchCounter);
 
     collectDefaultMetrics({ register });
     return register;
